d1a9c10756c47107c8caa0163e832f0b67da51d3
NumbuhFour <fad4470@rit.edu>
2015-02-16 12:33:40 -0500

    Implemented ++ upvoting system

--- commands/money.py
+++ commands/money.py
@@ -1,23 +1,31 @@
 from command import Command
 import json
+import time
 class Money(Command):
 	fileName = 'money.txt'
 	bank = {}
+	karmaTimer = {}
+	karmaPerVote = 1
+	voteDelay = 20
 	
 	def __init__(self, hb):
 		self.hb = hb
 		self.helpString = "*!give [target] [amount]: Grants target user x monies"
-		self.publicHelpString = "!balance: Prints out your current balance ######## !pay [target] [amount]: Pays target x monies from your account" # ######## [user]++ : Grant a user one point for being awesome"
-		#self.helpString = "!giveaway [viewers] [followers] [subscribers]: Pick a person for the giveaway";
+		self.publicHelpString = "!balance: Prints out your current balance ######## !pay [target] [amount]: Pays target x monies from your account ######## [user]++ : Grant a user " + str(self.karmaPerVote) + " point" + ("s" if self.karmaPerVote==1 else "") + " for being awesome"
+		self.helpString = "*!setUpvoteAmount [amount]: Sets the number of monies granted by [user]++ ######## *!setUpvoteDelay [seconds]: Sets how long you must wait between upvotes";
 		pass
 		
 	def writeConf(self, conf):
 		conf['money'] = {}
 		conf['money']['fileName'] = self.fileName
+		conf['money']['upvoteDelay'] = self.voteDelay
+		conf['money']['moneyPerVote'] = self.karmaPerVote
 		pass
 	
 	def readFromConf(self, conf):
 		self.fileName = conf['money']['fileName']
+		self.voteDelay = conf['money']['upvoteDelay']
+		self.karmaPerVote = conf['money']['moneyPerVote']
 		self.loadBankFile()
 		pass
 		
@@ -25,8 +33,14 @@ class Money(Command):
 		lower = message.strip().lower()
 		if lower.find('!bal') != -1 or lower.find('!pay ') == 0:
 			return True
+		elif lower.find('++') != -1:
+			return True;
 		elif self.hb.isOp(user) == True:
 			if lower.find('!give ') == 0:
+				return True
+			if lower.find('!setupvoteamount ') == 0:
+				return True
+			if lower.find('!setupvotedelay ') == 0:
 				return True
 				
 		return False
@@ -38,7 +52,22 @@ class Money(Command):
 				self.hb.message(user + ": Give me a sec, I just woke up!")
 			else:
 				self.hb.message(user + ": You currently have " + str(self.checkBalance(user)) + " monies.")
-		if lower.find('!pay ') == 0:
+		elif lower.find('++') != -1:
+			curTime = time.time()
+			if (user in self.karmaTimer) == False or curTime-self.karmaTimer[user] > self.voteDelay:
+				split = lower.split(' ')
+				for t in split:
+					if t.find('++') == (len(t)-2) and len(t) > 2: #Check if at end
+						name = t[:-2]
+						if name == user:
+							self.hb.message(user + ": You can't upvote yourself!")
+							break
+						elif self.checkBalance(name) != None:
+							self.pay(name, self.karmaPerVote)
+							self.karmaTimer[user] = curTime
+							break
+				pass
+		elif lower.find('!pay ') == 0:
 			split = msg.split(' ')
 			if len(split) == 3:
 				amt = split[2]
@@ -63,7 +92,7 @@ class Money(Command):
 		if(self.hb.isOp(user) == True):
 			if lower.find('!give ') == 0:
 				split = msg.split(' ')
-				if len(split) == 3:
+				if len(split) >= 3:
 					amt = split[2]
 					target = split[1]
 					try: 
@@ -76,7 +105,32 @@ class Money(Command):
 					except ValueError:
 						self.hb.message(user + ": Error giving money.")
 						pass
+			if lower.find('!setupvoteamount ') == 0:
+				split = msg.split(' ')
+				if len(split) >= 2:
+					amt = split[1]
+					try: 
+						amount = int(amt)
+						self.karmaPerVote = amount
+						self.hb.message(user + ": Upvote amount set to " + str(amount))
+						self.hb.saveSettings()
+					except ValueError:
+						self.hb.message(user + ": Error setting upvote amount.")
+						pass
+			if lower.find('!setupvotedelay ') == 0:
+				split = msg.split(' ')
+				if len(split) >= 2:
+					amt = split[1]
+					try: 
+						amount = int(amt)
+						self.voteDelay = amount
+						self.hb.message(user + ": Upvote delay set to " + str(amount))
+						self.hb.saveSettings()
+					except ValueError:
+						self.hb.message(user + ": Error setting upvote delay.")
+						pass
 		
+	
 	def checkBalance(self,user):
 		if(user.lower() in self.bank) == False and self.hb.isOnline(user) == True:
 			print user + " not found, adding"
