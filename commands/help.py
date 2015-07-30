dd7b7bd08807ffa0892b7b7d146a7e69258b2f23
NumbuhFour <fad4470@rit.edu>
2015-07-29 22:16:29 -0400

    Split mod help by topics

--- commands/help.py
+++ commands/help.py
@@ -21,19 +21,36 @@ class Help(Command):
 		
 	def onMessage(self, message, user):
 		output = ""
-		isOp = self.hb.isOp(user) and message.find("mod") != -1
-		for cmd in self.hb.commands:
-			pubHelp = cmd.publicHelpString
-			if isOp == True:
-				help = cmd.helpString
-				if help != "":
-					output += help + " ### "
-			elif pubHelp != "":
-				output += pubHelp + " ### "
-					
-			if(len(output) > 800):
-				self.hb.message(user + ": " + output)
-				output = ""
-				time.sleep(1.5)
+		isOp = self.hb.isOp(user)
+		modList = message.find("mod") != -1
+		topic = ""
+		split = message.lower().split(' ')
+		if(len(split) > 1 and not modList):
+			topic = split[1].strip()
+		if(isOp and modList):
+			output = "Type !help [topic] for more info. Available topics: "
+			for cmd in self.hb.commands:
+				if cmd.helpString != "":
+					output += cmd.getName().lower() + ", "
+			output = output[:-2]
+		elif(topic == "" or not isOp):
+			for cmd in self.hb.commands:
+				pubHelp = cmd.publicHelpString
+				if pubHelp != "":
+					output += pubHelp + " ### "
+						
+				if(len(output) > 800):
+					self.hb.message(user + ": " + output)
+					output = ""
+					time.sleep(1.5)
+		elif(topic != ""):
+			found = False
+			for cmd in self.hb.commands:
+				if cmd.getName().lower() == topic:
+					found = True
+					output = cmd.helpString + " ### " + cmd.publicHelpString
+					break
+			if( not found ):
+				output = "Topic '" + topic + "' not found."
 			
 		self.hb.message(user + ": " + output)
